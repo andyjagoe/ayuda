7ea31423a993a062493ad9a8afd17d7986b85624
Andy Jagoe <andy@jagoe.com>
2020-06-08 18:25:10 -0700

    fix task  handler in createjob listener

--- functions/jobCreatedTasks.js
+++ functions/jobCreatedTasks.js
@@ -52,7 +52,7 @@ async function getSnaps(uid, jobDoc, firestoreDb) {
 }
 
 
-exports.handler = async function(snapshot, context, firestoreDb, emailHandler) {
+exports.handler = async function(snapshot, context, firestoreDb, emailHandler, taskHandler) {
     const uid = context.params.uid;    
 
     var jobDoc = snapshot.data();

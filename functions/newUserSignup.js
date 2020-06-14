b1f96ecdf1a666286cde9936067651fbe9deaf32
Andy Jagoe <andy@jagoe.com>
2020-06-13 19:41:00 -0700

    create 0 rate as default, fix  start -> join  url  in change mtg  for client  email

--- functions/newUserSignup.js
+++ functions/newUserSignup.js
@@ -1,56 +1,56 @@
 
 
-exports.handler = function(event, firestoreDb, admin, zoomHelper, emailHandler) {
+exports.handler = async function(event, firestoreDb, admin, zoomHelper, emailHandler) {
     //console.log(event);
 
-    var zoomResponse = null
     const user = {
         uid: event.uid,
         name: event.displayName,
         email: event.email,
     }
 
-    const myAxios = zoomHelper.getAxiosWithInterceptor()
-    return myAxios({
-        method: 'post',
-        url: 'https://api.zoom.us/v2/users',
-        data: {
-            "action": "custCreate",
-            "user_info": {
-              "email": event.email,
-              "type": 1,
+    try {
+        const myAxios = zoomHelper.getAxiosWithInterceptor()
+        const zoomResponse = await myAxios({
+            method: 'post',
+            url: 'https://api.zoom.us/v2/users',
+            data: {
+                "action": "custCreate",
+                "user_info": {
+                  "email": event.email,
+                  "type": 1,
+                }
             }
-        }
-    })
-    .then((response) => {
-        //console.log('response data: ', response.data);
-        zoomResponse = response.data
-
-        return firestoreDb.collection("users").doc(event.uid).set({
+        })
+        
+        await firestoreDb.collection("users").doc(event.uid).set({
             displayName: event.displayName,
             email: event.email,    
             emailVerified: event.emailVerified,
             photoURL: event.photoURL,
-            zoomId: zoomResponse.id,
+            zoomId: zoomResponse.data.id,
             signedUpTime: admin.firestore.Timestamp.fromDate(new Date()),
         }, { merge: true });    
-    })
-    .then(response => {
+
+        await firestoreDb.collection('/users').doc(event.uid).collection('rates').add({
+            name: '0',
+            rate: parseFloat('0'),
+            currency: 'usd',
+            t: admin.firestore.Timestamp.fromDate(new Date()),    
+        })
+
         //console.log('Adding zoom mapping for webhook callbacks: ', response);
-        return firestoreDb.collection("zoom_map").doc(zoomResponse.id).set({
+        await firestoreDb.collection("zoom_map").doc(zoomResponse.data.id).set({
             uid: event.uid,
         });
-    })  
-    .then(response => {
+        
         //console.log('newUserSignup added to db: ', JSON.stringify(response));
-        return emailHandler.sendWelcomeEmail(user)
-    })  
-    .then(response => {
-        //console.log('newUserSignup success: ', JSON.stringify(response));
-        return true;
-    })  
-    .catch(error => {
-        console.error("newUserSignup Error: ", error);
-        return false;
-    });
+        await emailHandler.sendWelcomeEmail(user)
+
+        return true
+
+    } catch (error) {
+        console.error("Error: ", error);
+        return false
+    }
 }

4ead74bc96f5065e228919101054574b4794468d
Andy Jagoe <andy@jagoe.com>
2020-06-19 13:42:28 -0700

    profile  sharing for dekstop and non-mobile

--- src/views/ProfilePage.js
+++ src/views/ProfilePage.js
@@ -8,6 +8,16 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
+import IconButton from "@material-ui/core/IconButton";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import { SvgIcon } from '@material-ui/core';
+import copy from 'copy-to-clipboard';
+import Snackbar from '@material-ui/core/Snackbar';
 import { navigate } from "@reach/router"
 import { makeStyles } from '@material-ui/core/styles';
 import { UserContext } from "../providers/UserProvider";
@@ -15,6 +25,27 @@ import { ProfileContext } from "../providers/ProfileProvider";
 import {isMobile} from 'react-device-detect';
 import firebase from 'firebase/app';
 import 'firebase/functions';
+import {
+  EmailShareButton,
+  FacebookShareButton,
+  FacebookMessengerShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  EmailIcon,
+  FacebookIcon,
+  FacebookMessengerIcon,
+  LinkedinIcon,
+  TwitterIcon,
+} from "react-share";
+
+
+function CopyIcon(props) {
+  return (
+    <SvgIcon {...props}>
+      <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"/>
+    </SvgIcon>
+  );
+}
 
 
 const useStyles = makeStyles((theme) => ({  
@@ -41,6 +72,16 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.secondary.main,
     width: theme.spacing(15),
     height: theme.spacing(15),
+  },
+  shareavatar: {
+    width: theme.spacing(4),
+    height: theme.spacing(4),
+  },
+  sharedialogtext: {
+    margin: theme.spacing(0, 0, 1),
+  },
+  copydialogtext: {
+    margin: theme.spacing(1, 0, 0),
   },
   spacingFooter: {
     marginTop: theme.spacing(50),
@@ -138,24 +179,64 @@ const ProfilePage = (props) => {
 
   // Handle sharing
   const share = () => {
+
+    if (navigator.share && isMobile) {
+      navigator.share({
+        title: shareTitle(),
+        text: shareText(),
+        url: shareUrl(),
+      })
+        .then(() => console.log('Successful share'))
+        .catch((error) => console.log('Error sharing', error));
+    } else {
+      handleOpenDialog();
+    }
+  }
+
+  const shareUrl = () => {
     let url = document.location.href;
     const canonicalElement = document.querySelector('link[rel=canonical]');
     if (canonicalElement !== null) {
         url = canonicalElement.href;
     }
-
-    if (navigator.share && isMobile) {
-      navigator.share({
-        title: `${formatUserName()} - ${profileRecord.headline}`,
-        text: `${formatUserName()}: ${profileRecord.headline}\n\n${profileRecord.bio}`,
-        url: url,
-      })
-        .then(() => console.log('Successful share'))
-        .catch((error) => console.log('Error sharing', error));
-    } else {
-      console.log("Not implemented ")
-    }
+    return url
   }
+
+  const shareTitle = () =>  {
+    return `${formatUserName()} - ${profileRecord.headline}`
+  }
+  
+  const shareText = () =>  {
+    return `${formatUserName()}: ${profileRecord.headline}\n\n${profileRecord.bio}`
+  }
+
+
+  // Handle Dialog
+  const [openDialog, toggleOpenDialog] = useState(false);
+  const handleOpenDialog = () => {
+    toggleOpenDialog(true);
+  };
+  const handleCloseDialog = () => {
+    toggleOpenDialog(false);
+  };
+
+
+    // Handle copying and snackbar messages
+    const [openSnackbar, setOpenSnackbar] = useState(false);
+    const [snackbarMessage, setSnackbarMessage] = useState(false);
+    const [snackbarKey, setSnackbarKey] = useState(false);
+    const handleSnackbarClick = (text, message, key) => {
+      copy(text);
+      setSnackbarMessage(message);
+      setSnackbarKey(key);
+      setOpenSnackbar(true);
+    };
+    const handleSnackbarClose = (event, reason) => {
+      if (reason === 'clickaway') {
+        return;
+      }
+      setOpenSnackbar(false);
+    };
   
 
   return (
@@ -210,6 +291,76 @@ const ProfilePage = (props) => {
           </Typography>
         </div>
 
+        <Dialog open={openDialog} 
+            onClose={handleCloseDialog} 
+            aria-labelledby="form-dialog"
+            maxWidth="sm"
+          >
+            <DialogTitle 
+              id="form-dialog" 
+              onClose={handleCloseDialog}
+            >              
+              Profile for {formatUserName()}
+            </DialogTitle>
+            <DialogContent>
+                <Typography variant="body1" className={classes.sharedialogtext}>                  
+                  Share with one of these options
+                </Typography>            
+                <EmailShareButton url={shareUrl()} subject={shareTitle()} body={shareText()}>
+                  <EmailIcon size={64} round />
+                </EmailShareButton>
+                <FacebookShareButton url={shareUrl()} quote={shareTitle()}>
+                  <FacebookIcon size={64} round />
+                </FacebookShareButton>
+                <FacebookMessengerShareButton url={shareUrl()} appId={293795268691794}>
+                  <FacebookMessengerIcon size={64} round />
+                </FacebookMessengerShareButton>
+                <LinkedinShareButton url={shareUrl()}>
+                  <LinkedinIcon size={64} round />
+                </LinkedinShareButton>
+                <TwitterShareButton url={shareUrl()} title={shareTitle()}>
+                  <TwitterIcon size={64} round />
+                </TwitterShareButton>
+                <Typography variant="body1" className={classes.copydialogtext}>
+                  Or, share link
+                </Typography>
+                <TextField
+                  margin="dense"
+                  id="link"
+                  fullWidth
+                  variant="outlined"
+                  value={shareUrl()}
+                  disabled
+                  type="text"
+                  InputProps={{endAdornment:
+                    <InputAdornment>
+                      <IconButton
+                        onClick={() => { handleSnackbarClick(
+                          shareUrl(),
+                          'Share link copied',
+                          'share'); }}
+                      >
+                        <CopyIcon />                                                  
+                      </IconButton>
+                    </InputAdornment>
+                  }}
+                />          
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleCloseDialog} color="primary">
+                Done
+              </Button>
+            </DialogActions>
+        </Dialog>
+
+        <Snackbar
+          anchorOrigin= {{ vertical: 'top', horizontal: 'center' }}
+          key={snackbarKey}
+          autoHideDuration={3000}
+          open={openSnackbar}
+          onClose={handleSnackbarClose}
+          message={snackbarMessage}
+        />
 
         <div className={classes.spacingFooter} />
       </Container>  

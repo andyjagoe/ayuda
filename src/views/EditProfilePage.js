b78ba78d8f6edaedb4e8caee88085027cd23ee68
Andy Jagoe <andy@jagoe.com>
2020-06-21 13:51:08 -0700

    move avatar edit to EditoProfile page, fix bugs with avatar editor

--- src/views/EditProfilePage.js
+++ src/views/EditProfilePage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef, useContext } from "react";
+import React, { useEffect, useState, useRef, useContext, useCallback } from "react";
 import MenuAppBar from 'components/MenuAppBar';
 import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -7,17 +7,63 @@ import Link from '@material-ui/core/Link';
 import { navigate } from "@reach/router"
 import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
+import AvatarEditor from 'react-avatar-editor'
+import Dropzone from 'react-dropzone';
+import Slider from '@material-ui/core/Slider';
+import Alert from '@material-ui/lab/Alert';
+import Collapse from '@material-ui/core/Collapse';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import { ProfileContext } from "../providers/ProfileProvider";
 import { UserContext } from "../providers/UserProvider";
 import InputAdornment from '@material-ui/core/InputAdornment';
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from '@material-ui/icons/Close';
 import clsx from 'clsx';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { green } from '@material-ui/core/colors';
 import { makeStyles } from '@material-ui/core/styles';
 import { firestore } from "../firebase"
+import firebase from 'firebase/app';
+import 'firebase/functions';
+import axios from 'axios';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
+
+
+const styles = (theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+});
+
+
+const MyDialogTitle = withStyles(styles)((props) => {
+  const { children, classes, onClose, ...other } = props;
+  return (
+    <MuiDialogTitle disableTypography className={classes.root} {...other}>
+      <Typography variant="h6">{children}</Typography>
+      {onClose ? (
+        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
+});
 
 
 const useStyles = makeStyles((theme) => ({
@@ -32,7 +78,7 @@ const useStyles = makeStyles((theme) => ({
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: 'transparent',
     width: theme.spacing(15),
     height: theme.spacing(15),
   },
@@ -50,6 +96,12 @@ const useStyles = makeStyles((theme) => ({
     width: '100%',
   },
   buttonSuccess: {
+    backgroundColor: green[500],
+    '&:hover': {
+      backgroundColor: green[700],
+    },
+  },
+  buttonSuccessAvatar: {
     backgroundColor: green[500],
     '&:hover': {
       backgroundColor: green[700],
@@ -90,7 +142,9 @@ const EditProfilePage = () => {
   const [bio, setBio] = useState("");
   const [bioCounter, setBioCounter] = useState(0);
   const [shortId, setShortId] = useState("");
-  const [photoURL, setPhotoURL] = useState("");
+  //const [photoURL, setPhotoURL] = useState("");
+  const [avatarImage, setAvatarImage] = useState('')
+  const [avatarEditorImage, setAvatarEditorImage] = useState('')  
   const [hasLoadedProfile, setHasLoadedProfile] = useState(false)
 
   const [disable, setDisabled] = useState(true)
@@ -120,7 +174,7 @@ const EditProfilePage = () => {
       setBio(profile.bio || '')
       if ('bio' in profile) setBioCounter(profile.bio.length)
       setShortId(profile.shortId || '')
-      setPhotoURL(profile.photoURL || '')
+      getAvatarImage(profile.photoURL || '')
       setHasLoadedProfile(true)
     }
 
@@ -178,6 +232,153 @@ async function handleSubmit(event) {
   }
 }
 
+
+  // Handle Avatar
+  const [disableAvatar, setDisabledAvatar] = useState(false)
+  const [loadingAvatar, setLoadingAvatar] = useState(false);
+  const [successAvatar, setSuccessAvatar] = useState(false);
+  const buttonClassnameAvatar = clsx({
+    [classes.buttonSuccessAvatar]: successAvatar,
+  });
+
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('xs'));
+  const setEditorRef = useRef()
+  const [scale, setScale] = useState(1.2)
+
+  const handleScale = (event, newValue) => {
+    setScale(parseFloat(newValue))
+  }
+
+  const handleSave = async () => {
+    if (setEditorRef){
+      setShowAlert(false)
+      setDisabledAvatar(true)
+  
+      if (!loadingAvatar) {
+        setSuccessAvatar(false);
+        setLoadingAvatar(true);
+      }
+  
+      const canvasScaled = setEditorRef.current.getImageScaledToCanvas()
+      const dataUrl = canvasScaled.toDataURL()
+      var blobData = dataURItoBlob(dataUrl);
+
+      try {
+        // Get permission to upload
+        var getUploadAvatarUrl = firebase.functions().httpsCallable('getUploadAvatarUrl');
+        const result = await getUploadAvatarUrl()
+
+        // Upload to S3
+        let formData = new FormData();
+        formData.append("Content-Type", "image/png");
+        Object.entries(result.data.signedRequest.fields).forEach(([k, v]) => {
+	        formData.append(k, v);
+        });
+        formData.append("file", blobData);
+        await axios({
+          method: 'post',
+          url: result.data.signedRequest.url,
+          data: formData,
+          headers: {'Content-Type': 'multipart/form-data' }
+        })
+
+        // Update user profile
+        await firestore.collection('/users').doc(user.uid).set({
+          photoURL: result.data.url,
+        }, { merge: true });
+
+        getAvatarImage(profile.photoURL || '')
+        setDisabledAvatar(false)
+        setSuccessAvatar(true);
+        setLoadingAvatar(false);  
+        handleCloseAvatarDialog()
+
+      } catch (error) {
+        console.error(error);
+        setDisabledAvatar(false)
+        setSuccessAvatar(false);
+        setLoadingAvatar(false);
+      }    
+    }
+
+  }
+
+  const getAvatarImage = async (url) => {
+    const src = await getDataImage(url)
+    setAvatarImage(src)
+  }
+
+  const getDataImage = async (url) => {
+    const response = await fetch(url, { cache: 'no-store' });
+    const contentType = response.headers.get("content-type")
+    const buffer = await response.arrayBuffer();
+
+    let binary = '';
+    const bytes = [].slice.call(new Uint8Array(buffer));
+    bytes.forEach(b => binary += String.fromCharCode(b));
+    const base64 = window.btoa(binary);
+
+    return `data:${contentType};base64,${base64}`;
+  }
+
+  const dataURItoBlob = (dataURI) => {
+    var binary = atob(dataURI.split(',')[1]);
+    var array = [];
+    for(var i = 0; i < binary.length; i++) {
+        array.push(binary.charCodeAt(i));
+    }
+    return new Blob([new Uint8Array(array)], {type: 'image/png'});
+  }
+
+  const onDrop = useCallback((acceptedFiles) => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader()
+
+      reader.onabort = () => console.log('file reading was aborted')
+      reader.onerror = () => console.log('file reading has failed')
+      reader.onload = () => {
+        const binaryStr = reader.result
+      }
+      setAvatarEditorImage(file)
+    })    
+  }, [])
+
+  const onFilesRejected = useCallback((fileRejections) => {
+    if (fileRejections.length > 1)  {
+      setAlertMessage(`Only one file may be added`)
+      setShowAlert(true)       
+    } else if (fileRejections[0].errors[0].code === 'file-invalid-type') {
+      setAlertMessage('Only JPEG and PNG files supported')
+      setShowAlert(true)  
+    } else if(fileRejections[0].file.size > avatarMaxSize) {
+      const sizeInMB = (avatarMaxSize / (1024*1024)).toFixed(2);
+      setAlertMessage(`Maximum file size is ${sizeInMB} MB`)
+      setShowAlert(true)   
+    }
+  }, [])
+
+  const avatarMaxSize = 5242880;
+  const [alertMessage, setAlertMessage] = useState('Error');
+  const [showAlert, setShowAlert] = useState(false);
+
+  const [openAvatarDialog, toggleOpenAvatarDialog] = useState(false);
+  const handleOpenAvatarDialog = () => {  
+    setAvatarEditorImage(avatarImage)
+    setShowAlert(false)
+    toggleOpenAvatarDialog(true);
+    const hubspot = document.getElementById('hubspot-messages-iframe-container')
+    if (hubspot != null)  hubspot.style.setProperty('display', 'none', 'important');
+  };
+
+  const handleCloseAvatarDialog = () => {    
+    setSuccessAvatar(false);
+    toggleOpenAvatarDialog(false);
+    const hubspot = document.getElementById('hubspot-messages-iframe-container')
+    if (hubspot != null)  hubspot.style.display = 'initial';
+  };
+  
+
   return (
     <React.Fragment>
       <MenuAppBar />
@@ -194,7 +395,11 @@ async function handleSubmit(event) {
         </Breadcrumbs>
 
         <div className={classes.paper}>
-            <Avatar className={classes.avatar} src={photoURL} />
+            <Avatar 
+              className={classes.avatar} 
+              src={avatarImage} 
+              onClick={() => {handleOpenAvatarDialog() }}
+            />
             <Typography component="h1" variant="h5">
               Edit Profile
             </Typography>
@@ -304,6 +509,119 @@ async function handleSubmit(event) {
               </form>
 
           </div>
+
+          <Dialog open={openAvatarDialog} 
+            onClose={handleCloseAvatarDialog} 
+            aria-labelledby="form-dialog-avatar"
+            maxWidth="sm"
+            fullScreen={fullScreen}
+          >
+            <MyDialogTitle 
+              id="form-dialog-avatar" 
+              onClose={handleCloseAvatarDialog}
+            >              
+              Edit photo
+            </MyDialogTitle>
+            
+            <DialogContent>
+              <Collapse in={showAlert}>
+                <Alert severity="error">
+                  {alertMessage}
+                </Alert>
+              </Collapse>
+
+              <Dropzone 
+                name='zone1'
+                noClick 
+                onDrop={onDrop}
+                onDropRejected={onFilesRejected}
+                accept='image/jpeg, image/png'
+                minSize={0}
+                maxSize={avatarMaxSize}
+                multiple={false}
+              >
+                {({getRootProps, getInputProps, isDragActive, isDragReject}) => {
+                  if (isDragActive && !isDragReject) setShowAlert(false);
+                  return  (
+                    <section>
+                    <div {...getRootProps()}>
+                      <input {...getInputProps()} />
+                      <AvatarEditor
+                        ref={setEditorRef}
+                        image={avatarEditorImage}
+                        crossOrigin='anonymous'
+                        width={250}
+                        height={250}
+                        border={50}
+                        borderRadius={125}
+                        color={[255, 255, 255, 0.6]} // RGBA
+                        scale={scale}
+                        rotate={0}
+                      />                  
+                    </div>
+                  </section>
+                  )
+                }}
+              </Dropzone>      
+              <Slider
+                name="scale"
+                defaultValue={1.2}
+                aria-labelledby="discrete-slider"
+                valueLabelDisplay="auto"
+                step={0.1}
+                min={1}
+                max={2}
+                onChange={handleScale}                
+              />
+            </DialogContent>
+            <DialogActions>
+              <Dropzone 
+                name='zone2'
+                noDrag
+                onDrop={onDrop}
+                onDropRejected={onFilesRejected}
+                accept='image/jpeg, image/png'
+                minSize={0}
+                maxSize={avatarMaxSize}
+                multiple={false}
+              >
+                {({getRootProps, getInputProps, isDragActive, isDragReject}) => {
+                  if (isDragActive && !isDragReject) setShowAlert(false);
+                  return (
+                    <section>
+                      <div {...getRootProps()}>
+                        <input {...getInputProps()} />
+                          <div className={classes.root}>
+                          <div className={classes.wrapper}>
+                            <Button color="primary">
+                              Change Photo
+                            </Button>
+                          </div>
+                        </div>
+                      </div>
+                    </section>
+                  )
+                }}
+              </Dropzone>
+              <div className={classes.root}>
+                <div className={classes.wrapper}>
+                  <Button
+                    type="submit"
+                    color="primary"
+                    className={buttonClassnameAvatar}
+                    disabled={disableAvatar}
+                    onClick={handleSave}
+                  >
+                    Save
+                  </Button>
+                  {loadingAvatar && <CircularProgress size={24} className={classes.buttonProgress} />}
+                </div>
+              </div>
+
+
+            </DialogActions>
+        </Dialog>
+
           <div className={classes.spacingFooter} />
 
       </Container>  
--- src/views/ProfilePage.js
+++ src/views/ProfilePage.js
@@ -1,25 +1,17 @@
-import React, { useEffect, useState, useRef, useContext, useCallback } from "react";
+import React, { useEffect, useState, useRef, useContext } from "react";
 import PublicAppBar from 'components/PublicAppBar';
 import MenuAppBar from 'components/MenuAppBar';
 import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 import Avatar from '@material-ui/core/Avatar';
-import AvatarEditor from 'react-avatar-editor'
-import Dropzone from 'react-dropzone';
-import Slider from '@material-ui/core/Slider';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogActions from '@material-ui/core/DialogActions';
-import MuiDialogTitle from '@material-ui/core/DialogTitle';
-import { withStyles } from '@material-ui/core/styles';
-import CloseIcon from '@material-ui/icons/Close';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import Alert from '@material-ui/lab/Alert';
-import Collapse from '@material-ui/core/Collapse';
 import TextField from '@material-ui/core/TextField';
 import IconButton from "@material-ui/core/IconButton";
 import InputAdornment from "@material-ui/core/InputAdornment";
@@ -28,15 +20,11 @@ import copy from 'copy-to-clipboard';
 import Snackbar from '@material-ui/core/Snackbar';
 import { navigate } from "@reach/router"
 import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import { green } from '@material-ui/core/colors';
 import { UserContext } from "../providers/UserProvider";
 import { ProfileContext } from "../providers/ProfileProvider";
 import {isMobile} from 'react-device-detect';
 import firebase from 'firebase/app';
 import 'firebase/functions';
-import { firestore } from "../firebase"
 import {
   EmailShareButton,
   FacebookShareButton,
@@ -49,9 +37,6 @@ import {
   LinkedinIcon,
   TwitterIcon,
 } from "react-share";
-import axios from 'axios';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { useTheme } from '@material-ui/core/styles';
 
 
 function CopyIcon(props) {
@@ -62,34 +47,6 @@ function CopyIcon(props) {
   );
 }
 
-
-const styles = (theme) => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(2),
-  },
-  closeButton: {
-    position: 'absolute',
-    right: theme.spacing(1),
-    top: theme.spacing(1),
-    color: theme.palette.grey[500],
-  },
-});
-
-
-const MyDialogTitle = withStyles(styles)((props) => {
-  const { children, classes, onClose, ...other } = props;
-  return (
-    <MuiDialogTitle disableTypography className={classes.root} {...other}>
-      <Typography variant="h6">{children}</Typography>
-      {onClose ? (
-        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
-          <CloseIcon />
-        </IconButton>
-      ) : null}
-    </MuiDialogTitle>
-  );
-});
 
 
 const useStyles = makeStyles((theme) => ({  
@@ -130,29 +87,6 @@ const useStyles = makeStyles((theme) => ({
   spacingFooter: {
     marginTop: theme.spacing(50),
   },
-  root: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-  wrapper: {
-    margin: theme.spacing(3, 0, 2),
-    position: 'relative',
-    width: '100%',
-  },
-  buttonSuccess: {
-    backgroundColor: green[500],
-    '&:hover': {
-      backgroundColor: green[700],
-    },
-  },
-  buttonProgress: {
-    color: green[500],
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    marginTop: -12,
-    marginLeft: -12,
-  },  
 }));
 
 
@@ -219,7 +153,7 @@ const ProfilePage = (props) => {
         if (isCancelled === false) {
           setProfileRecord(profile);
           setProfileShortId(props.shortId)
-          getAvatarImage(profile.photoURL)
+          setAvatarImage(profile.photoURL)
           setMemberSince(`joined ${result.data.memberSince}` || '')
         }
     } catch (error) {
@@ -250,7 +184,6 @@ const ProfilePage = (props) => {
 
   // Handle sharing
   const share = () => {
-
     if (navigator.share && isMobile) {
       navigator.share({
         title: shareTitle(),
@@ -282,143 +215,6 @@ const ProfilePage = (props) => {
   }
 
 
-
-
-  // Handle Avatar
-  const theme = useTheme();
-  const fullScreen = useMediaQuery(theme.breakpoints.down('xs'));
-  const setEditorRef = useRef()
-  const [scale, setScale] = useState(1.2)
-
-  const handleScale = (event, newValue) => {
-    setScale(parseFloat(newValue))
-  }
-
-  const handleSave = async () => {
-    if (setEditorRef){
-      setShowAlert(false)
-      setDisabled(true)
-  
-      if (!loading) {
-        setSuccess(false);
-        setLoading(true);
-      }
-  
-      const canvasScaled = setEditorRef.current.getImageScaledToCanvas()
-      const dataUrl = canvasScaled.toDataURL()
-      var blobData = dataURItoBlob(dataUrl);
-
-      try {
-        // Get permission to upload
-        var getUploadAvatarUrl = firebase.functions().httpsCallable('getUploadAvatarUrl');
-        const result = await getUploadAvatarUrl()
-
-        // Upload to S3
-        let formData = new FormData();
-        formData.append("Content-Type", "image/png");
-        Object.entries(result.data.signedRequest.fields).forEach(([k, v]) => {
-	        formData.append(k, v);
-        });
-        formData.append("file", blobData);
-        await axios({
-          method: 'post',
-          url: result.data.signedRequest.url,
-          data: formData,
-          headers: {'Content-Type': 'multipart/form-data' }
-        })
-
-        // Update user profile
-        await firestore.collection('/users').doc(user.uid).set({
-          photoURL: result.data.url,
-        }, { merge: true });
-
-        fetchData();
-        setDisabled(false)
-        setSuccess(true);
-        setLoading(false);  
-        handleCloseAvatarDialog()
-
-      } catch (error) {
-        console.error(error);
-        setDisabled(false)
-        setSuccess(false);
-        setLoading(false);
-      }    
-    }
-
-  }
-
-  const getAvatarImage = async (url) => {
-    const response = await fetch(url, { cache: 'no-store' });
-    const buffer = await response.arrayBuffer();
-
-    let binary = '';
-    const bytes = [].slice.call(new Uint8Array(buffer));
-    bytes.forEach(b => binary += String.fromCharCode(b));
-    const base64 = window.btoa(binary);
-
-    const src = `data:image/jpeg;base64,${base64}`;
-
-    setAvatarImage(src)
-  }
-
-  const dataURItoBlob = (dataURI) => {
-    var binary = atob(dataURI.split(',')[1]);
-    var array = [];
-    for(var i = 0; i < binary.length; i++) {
-        array.push(binary.charCodeAt(i));
-    }
-    return new Blob([new Uint8Array(array)], {type: 'image/png'});
-  }
-
-  const onDrop = useCallback((acceptedFiles) => {
-    acceptedFiles.forEach((file) => {
-      const reader = new FileReader()
-
-      reader.onabort = () => console.log('file reading was aborted')
-      reader.onerror = () => console.log('file reading has failed')
-      reader.onload = () => {
-        const binaryStr = reader.result
-      }
-      //reader.readAsArrayBuffer(file)
-      setAvatarImage(file)
-    })    
-  }, [])
-
-  const onFilesRejected = useCallback((fileRejections) => {
-    if (fileRejections.length > 1)  {
-      setAlertMessage(`Only one file may be added`)
-      setShowAlert(true)       
-    } else if (fileRejections[0].errors[0].code === 'file-invalid-type') {
-      setAlertMessage('Only JPEG and PNG files supported')
-      setShowAlert(true)  
-    } else if(fileRejections[0].file.size > avatarMaxSize) {
-      const sizeInMB = (avatarMaxSize / (1024*1024)).toFixed(2);
-      setAlertMessage(`Maximum file size is ${sizeInMB} MB`)
-      setShowAlert(true)   
-    }
-  }, [])
-
-  const avatarMaxSize = 5242880;
-  const [alertMessage, setAlertMessage] = useState('Error');
-  const [showAlert, setShowAlert] = useState(false);
-
-  const [openAvatarDialog, toggleOpenAvatarDialog] = useState(false);
-  const handleOpenAvatarDialog = () => {    
-    setShowAlert(false)
-    toggleOpenAvatarDialog(true);
-    const hubspot = document.getElementById('hubspot-messages-iframe-container')
-    if (hubspot != null)  hubspot.style.setProperty('display', 'none', 'important');
-  };
-
-  const handleCloseAvatarDialog = () => {    
-    setSuccess(false);
-    toggleOpenAvatarDialog(false);
-    const hubspot = document.getElementById('hubspot-messages-iframe-container')
-    if (hubspot != null)  hubspot.style.display = 'initial';
-  };
-  
-
   // Handle Share Dialog
   const [openDialog, toggleOpenDialog] = useState(false);
   const handleOpenDialog = () => {
@@ -445,14 +241,6 @@ const ProfilePage = (props) => {
     }
     setOpenSnackbar(false);
   };
-
-  // Handle save loading animation
-  const [disable, setDisabled] = useState(false)
-  const [loading, setLoading] = useState(false);
-  const [success, setSuccess] = useState(false);
-  const buttonClassname = clsx({
-    [classes.buttonSuccess]: success,
-  });
  
 
   return (
@@ -473,7 +261,6 @@ const ProfilePage = (props) => {
           <Avatar 
             className={classes.avatar} 
             src={avatarImage} 
-            onClick={() => {if(isUsersProfile()) handleOpenAvatarDialog() }}
           />
 
           <Typography component="h1" variant="h4">
@@ -571,118 +358,6 @@ const ProfilePage = (props) => {
               <Button onClick={handleCloseDialog} color="primary">
                 Done
               </Button>
-            </DialogActions>
-        </Dialog>
-
-        <Dialog open={openAvatarDialog} 
-            onClose={handleCloseAvatarDialog} 
-            aria-labelledby="form-dialog-avatar"
-            maxWidth="sm"
-            fullScreen={fullScreen}
-          >
-            <MyDialogTitle 
-              id="form-dialog-avatar" 
-              onClose={handleCloseAvatarDialog}
-            >              
-              Edit photo
-            </MyDialogTitle>
-            
-            <DialogContent>
-              <Collapse in={showAlert}>
-                <Alert severity="error">
-                  {alertMessage}
-                </Alert>
-              </Collapse>
-
-              <Dropzone 
-                name='zone1'
-                noClick 
-                onDrop={onDrop}
-                onDropRejected={onFilesRejected}
-                accept='image/jpeg, image/png'
-                minSize={0}
-                maxSize={avatarMaxSize}
-                multiple={false}
-              >
-                {({getRootProps, getInputProps, isDragActive, isDragReject}) => {
-                  if (isDragActive && !isDragReject) setShowAlert(false);
-                  return  (
-                    <section>
-                    <div {...getRootProps()}>
-                      <input {...getInputProps()} />
-                      <AvatarEditor
-                        ref={setEditorRef}
-                        image={avatarImage}
-                        crossOrigin='anonymous'
-                        width={250}
-                        height={250}
-                        border={50}
-                        borderRadius={125}
-                        color={[255, 255, 255, 0.6]} // RGBA
-                        scale={scale}
-                        rotate={0}
-                      />                  
-                    </div>
-                  </section>
-                  )
-                }}
-              </Dropzone>      
-              <Slider
-                name="scale"
-                defaultValue={1.2}
-                aria-labelledby="discrete-slider"
-                valueLabelDisplay="auto"
-                step={0.1}
-                min={1}
-                max={2}
-                onChange={handleScale}                
-              />
-            </DialogContent>
-            <DialogActions>
-              <Dropzone 
-                name='zone2'
-                noDrag
-                onDrop={onDrop}
-                onDropRejected={onFilesRejected}
-                accept='image/jpeg, image/png'
-                minSize={0}
-                maxSize={avatarMaxSize}
-                multiple={false}
-              >
-                {({getRootProps, getInputProps, isDragActive, isDragReject}) => {
-                  if (isDragActive && !isDragReject) setShowAlert(false);
-                  return (
-                    <section>
-                      <div {...getRootProps()}>
-                        <input {...getInputProps()} />
-                          <div className={classes.root}>
-                          <div className={classes.wrapper}>
-                            <Button color="primary">
-                              Change Photo
-                            </Button>
-                          </div>
-                        </div>
-                      </div>
-                    </section>
-                  )
-                }}
-              </Dropzone>
-              <div className={classes.root}>
-                <div className={classes.wrapper}>
-                  <Button
-                    type="submit"
-                    color="primary"
-                    className={buttonClassname}
-                    disabled={disable}
-                    onClick={handleSave}
-                  >
-                    Save
-                  </Button>
-                  {loading && <CircularProgress size={24} className={classes.buttonProgress} />}
-                </div>
-              </div>
-
-
             </DialogActions>
         </Dialog>
 
